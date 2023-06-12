c79f6e4997fc48989c8a071a1eda1d73b144d82d
Haicang Zhang <zhanghaicnag@gmail.com>
2023-06-12 09:54:07 +0800

    setup.py works. :)

--- setup.py
+++ setup.py
@@ -1,29 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'AbFold',
-  version = '1.0.0',
-  license='MIT',
-  description = 'AbFold - AI based methods for antibody structure prediction',
-  author = 'Haicang Zhang',
-  author_email = 'zhanghaicang@gmail.com',
-  keywords = [
-    'artificial intelligence',
-    'attention mechanism',
-    'protein folding'
-  ],
-  
-  packages=['abfold'],
-  package_dir={'abfold': './abfold'},
-  package_data={'abfold': ['./abfold/common/new_rigid_schema.json']},
+    name = 'AbFold',
+    version = '2.0.0',
+    license='MIT',
+    description = 'AbFold - AI based methods for antibody structure prediction',
+    author = 'Haicang Zhang',
+    author_email = 'zhanghaicang@gmail.com',
+    
+    packages=find_packages(include=['abfold', 'abfold/*']),
+    include_package_data=True,
+    package_data={
+        'abfold': ['common/default_rigids.json', 'common/stereo_chemical_props.txt'],
+    },
   
   install_requires=[],
-  test_suite = 'tests',
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-  ],
 )

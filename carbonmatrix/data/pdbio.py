c0b1567278fd48bf1ffd56d2693598c2bf2d8fe2
Haicang Zhang <zhanghaicang@gmail.com>
2023-09-04 12:34:56 +0800

    cleanup code; move and rename

--- carbonmatrix/data/pdbio.py
+++ carbonmatrix/data/pdbio.py
@@ -1,4 +1,3 @@
-
 import logging
 import os
 
@@ -14,72 +13,6 @@ import torch
 from torch.nn import functional as F
 
 from carbonmatrix.common import protein, residue_constants
-from carbonmatrix.utils import exists
-
-def pad_for_batch(items, batch_length, dtype):
-    """Pad a list of items to batch_length using values dependent on the item type.
-
-    Args:
-        items: List of items to pad (i.e. sequences or masks represented as arrays of
-            numbers, angles, coordinates, pssms).
-        batch_length: The integer maximum length of any of the items in the input. All
-            items are padded so that their length matches this number.
-        dtype: A string ('seq', 'msk', 'crd') reperesenting the type of
-            data included in items.
-
-    Returns:
-         A padded list of the input items, all independently converted to Torch tensors.
-    """
-    batch = []
-    if dtype == 'seq':
-        for seq in items:
-            z = torch.ones(batch_length - seq.shape[0], dtype=seq.dtype) * residue_constants.unk_restype_index
-            c = torch.cat((seq, z), dim=0)
-            batch.append(c)
-    elif dtype == 'msk':
-        # Mask sequences (1 if present, 0 if absent) are padded with 0s
-        for msk in items:
-            z = torch.zeros(batch_length - msk.shape[0], dtype=msk.dtype, device=msk.device)
-            c = torch.cat((msk, z), dim=0)
-            batch.append(c)
-    elif dtype == "crd":
-        for item in items:
-            z = torch.zeros((batch_length - item.shape[0],  item.shape[-2], item.shape[-1]), dtype=item.dtype)
-            c = torch.cat((item, z), dim=0)
-            batch.append(c)
-    elif dtype == "crd_msk":
-        for item in items:
-            z = torch.zeros((batch_length - item.shape[0],  item.shape[-1]), dtype=item.dtype)
-            c = torch.cat((item, z), dim=0)
-            batch.append(c)
-    elif dtype == "ebd":
-        for item in items:
-            shape = [batch_length - item.shape[0]] + list(item.shape[1:])
-            z = torch.zeros(shape, dtype=item.dtype,
-device = item.device)
-            c = torch.cat((item, z), dim=0)
-            batch.append(c)
-    elif dtype == "pair":
-        for item in items:
-            c = F.pad(item, (0, 0, 0, batch_length - item.shape[-2], 0, batch_length - item.shape[-2]))
-            batch.append(c)
-    else:
-        raise ValueError('Not implemented yet!')
-    batch = torch.stack(batch, dim=0)
-    return batch
-
-def weights_from_file(filename):
-    if filename:
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in filter(lambda x: len(x)>0, map(lambda x: x.strip(), f)):
-                items = line.split()
-                yield float(items[0])
-
-def embedding_get_labels(name, mat):
-    if name == 'token':
-        return [residue_constants.restypes_with_x[i if i < len(residue_constants.restypes_with_x) else -1]
-                for i in range(mat.shape[0])]
-    return None
 
 def pdb_save(step, batch, headers, prefix='/tmp', is_training=False):
     
